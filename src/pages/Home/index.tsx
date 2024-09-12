380ab47d3e0995f646cb25576eb10a7501c92c8e
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-09-12 10:44:46 -0300

    create integrations in landingpage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/Hero";
+import Integrations from "@/components/Integrations";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center min-h-screen"> {/* Ajuste para centrar */}
       <Hero />
+      <div className="integrations flex justify-center items-center w-full">
+        <Integrations />
+      </div>
     </div>
   );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import NavComponent from "./components/Nav";
 import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
@@ -8,13 +13,11 @@ function App() {
     <>
       <Router>
         <NavComponent />
-        <div className="mx-auto container">
-          <Routes>
-            <Route path="/iniciar-sesion" element={<LoginPage />} />
-            <Route path="/" element={<Navigate to="/inicio" />} />
-            <Route path="/inicio" element={<HomePage />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/iniciar-sesion" element={<LoginPage />} />
+          <Route path="/" element={<Navigate to="/inicio" />} />
+          <Route path="/inicio" element={<HomePage />} />
+        </Routes>
       </Router>
     </>
   );
