6d5fd486870b2286f75cd66afb469b60e2f52612
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-09-12 17:28:43 -0300

    acomodo el sidebar dentro de dashboard y otras paginas

--- src/routes.tsx
+++ src/routes.tsx
@@ -10,34 +10,39 @@ import LoginPage from "./pages/Login";
 import HomePage from "./pages/Home";
 import RegisterPage from "./pages/Register";
 import DashboardPage from "./pages/Dashboard";
+import { Sidebar } from "./components/Sidebar/Sidebar";
 
-// Componente que se encarga de verificar si mostrar o no el NavComponent
-function LayoutWithNav() {
+// Componente que se encarga de verificar si mostrar o no el NavComponent y el Sidebar
+function LayoutWithNavAndSidebar() {
   const location = useLocation();
   const noNavRoutes = ["/dashboard", "/campañas", "/calendarios", "/tareas"];
   const hideNav = noNavRoutes.includes(location.pathname);
 
   return (
-    <>
+    <div style={{ display: "flex" }}>
       {!hideNav && <NavComponent />}
-      <Routes>
-        <Route path="/iniciar-sesion" element={<LoginPage />} />
-        <Route path="/registrate" element={<RegisterPage />} />
-        <Route path="/" element={<Navigate to="/inicio" />} />
-        <Route path="/inicio" element={<HomePage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        {/* <Route path="/campañas" element={<CampaignsPage />} />
-        <Route path="/calendarios" element={<CalendarPage />} />
-        <Route path="/tareas" element={<TasksPage />} /> */}
-      </Routes>
-    </>
+      {hideNav && <Sidebar />}
+      
+      <div style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/iniciar-sesion" element={<LoginPage />} />
+          <Route path="/registrate" element={<RegisterPage />} />
+          <Route path="/" element={<Navigate to="/inicio" />} />
+          <Route path="/inicio" element={<HomePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          {/* <Route path="/campañas" element={<CampaignsPage />} />
+          <Route path="/calendarios" element={<CalendarPage />} />
+          <Route path="/tareas" element={<TasksPage />} /> */}
+        </Routes>
+      </div>
+    </div>
   );
 }
 
 function App() {
   return (
     <Router>
-      <LayoutWithNav />
+      <LayoutWithNavAndSidebar />
     </Router>
   );
 }
