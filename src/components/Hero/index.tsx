d4cd9629a66799829553fea7aeb0d20899955323
eliasgiannantonio <giannantonioelias@gmail.com>
2024-09-12 11:28:01 -0300

    Sección patrocinadores + arreglos en el hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
-    <div className="min-h-screen text-white">
+    <div className="text-white lg:min-h-screen">
       <div className="relative isolate">
         {/* Contenido del Hero */}
-        <div className="flex flex-col items-center px-4 py-10 text-center sm:py-32 lg:pb-20">
+        <div className="flex flex-col items-center px-4 pt-8 text-center sm:pt-10 lg:pt-20">
           <h1 className="text-4xl font-bold text-white sm:text-6xl">
             Centraliza y optimiza tu tiempo
           </h1>
@@ -33,11 +33,11 @@ export default function Hero() {
           </div>
 
           {/* Dashboard Image */}
-          <div className="w-full max-w-6xl">
+          <div className="w-full max-w-6xl overflow-hidden max-h-[730px]">
             <img
               src="src/assets/web_dashboard.svg"
               alt="App screenshot"
-              className="relative rounded-md bottom-10"
+              className="relative object-cover rounded-md bottom-5"
             />
           </div>
         </div>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import Patrocinadores from "@/components/Patrocinadores";
 
 const HomePage = () => {
   return (
     <div>
       <Hero />
+      <Patrocinadores/>
     </div>
   );
 };
