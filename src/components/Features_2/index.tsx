403a84203bd3bdf53dc88b3135e70dfc9f9b763b
eliasgiannantonio <giannantonioelias@gmail.com>
2024-09-13 17:29:04 -0300

    Creacion de componente de features

--- src/components/Features_2/index.tsx
+++ src/components/Features_2/index.tsx
@@ -1,11 +1,13 @@
 const Features_2 = () => {
   return (
-    <div className="max-w-[100vw]">
+    <div className="max-w-[100vw] mx-auto px-8">
+      {" "}
       <h3 className="my-20 text-5xl font-bold text-center text-blue-950">
         We make it easy to track all data analytics
       </h3>
-      <div className="flex flex-col items-center gap-10">
-        <div className="flex max-h-[370px] justify-between items-center px-28">
+      <div className="flex flex-col gap-20 pb-12">
+        {" "}
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20">
           <div className="flex flex-col items-start gap-3 max-w-[400px]">
             <img
               src="src/assets/features-2/Icon1-l.svg"
@@ -27,14 +29,13 @@ const Features_2 = () => {
           <div className="flex items-center">
             <img
               src="src/assets/features-2/Illustration1-r.svg"
-              className="w-auto h-auto"
+              className="w-auto h-auto max-w-[400px]"
               alt="Illustration"
             />
           </div>
         </div>
-
-        <div className="flex max-h-[370px] justify-between items-center px-28 flex-row-reverse">
-          <div className="flex flex-col items-start gap-3 max-w-[400px]">
+        <div className="flex flex-col-reverse md:flex-row-reverse justify-center items-center gap-10 md:gap-20">
+          <div className="flex flex-col items-start gap-3 max-w-[400px] relative sm:left-12">
             <img
               src="src/assets/features-2/Icon2-r.svg"
               alt="Icon"
@@ -55,13 +56,12 @@ const Features_2 = () => {
           <div className="flex items-center">
             <img
               src="src/assets/features-2/Illustration2-l.svg"
-              className="w-auto h-auto"
+              className="w-auto h-auto max-w-[400px] relative right-14"
               alt="Illustration"
             />
           </div>
         </div>
-
-        <div className="flex max-h-[370px] justify-between items-center px-28">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20">
           <div className="flex flex-col items-start gap-3 max-w-[400px]">
             <img
               src="src/assets/features-2/Icon3-l.svg"
@@ -83,7 +83,7 @@ const Features_2 = () => {
           <div className="flex items-center">
             <img
               src="src/assets/features-2/Illustration3-r.svg"
-              className="w-auto h-auto"
+              className="w-auto h-auto max-w-[400px]"
               alt="Illustration"
             />
           </div>
