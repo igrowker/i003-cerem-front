869851375d47345cb41462b2adb6a3fcdc854855
leandro mortarini <leandromortarini2@gmail.com>
2024-09-13 16:43:09 -0300

    styled: se puede cerrar el menu del dashboard

---
+++ src/components/Sidebar/LogoSidebar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+// Definir una interfaz para los props que el componente recibirá
+interface LogoSideBarProps {
+  isOpen: boolean;
+  url: string;
+}
+
+export const LogoSideBar: React.FC<LogoSideBarProps> = ({ isOpen, url }) => {
+  return (
+    <Link to={url}>
+      {isOpen ? (
+        <div className="w-full h-20 mt-5 flex justify-start">
+          <img src="src/assets/isotipo.svg" alt="" className="w-10" />
+
+          <img src="src/assets/logotipo_w.svg" alt="" className="h-20 " />
+        </div>
+      ) : (
+        <div className="w-full h-20 mt-5 flex justify-center">
+          <img src="src/assets/isotipo.svg" alt="Isotipo" className="w-10" />
+        </div>
+      )}
+    </Link>
+  );
+};
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineHome } from "react-icons/ai";
 import { IoIosCalendar, IoMdLogOut, IoMdSettings } from "react-icons/io";
 import { FaListUl, FaUsers } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { Button } from "../ui/button";
+import { ButtonSideBar } from "./ButtonSidebar";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+import { LogoSideBar } from "./LogoSidebar";
 
 export const Sidebar: React.FC = () => {
-  // const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  // const toggleSidebar = () => {
-  //   setIsOpen(!isOpen);
-  // };
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   // cyanLight: "#5CE1E6",
   // cyanDark: "#0097B2",
@@ -20,64 +22,83 @@ export const Sidebar: React.FC = () => {
 
   return (
     <>
-      <div className="w-[300px] h-screen bg-navy border-r-4  flex flex-col items-center justify-between ">
+      <div
+        className={` ${
+          isOpen ? "w-[300px]" : "w-24"
+        } transition-all duration-500 ease-in-out} w-[300px] h-screen bg-navy border-r-4  flex flex-col items-center justify-between `}
+      >
         <div className="w-3/4">
-          <div className="w-full h-20 mt-5 flex justify-start">
-            <img src="src/assets/isotipo.svg" alt="" className="w-10" />
-            <img src="src/assets/logotipo_w.svg" alt="" className="h-20 " />
-          </div>
+          <LogoSideBar isOpen={isOpen} url="/dashboard" />
           <div className="w-full h-[200px] flex flex-col justify-evenly ">
-            <Link to="/dashboard">
-              <span className="w-full h-10  flex items-center text-xl font-medium  text-blueGray hover:bg-cyanDark hover:text-lightGray p-2 rounded-lg">
-                <AiOutlineHome className="mr-2" /> Dashboard
-              </span>
-            </Link>
-            <Link to="/calendarios">
-              <span className="w-full h-10  flex items-center text-xl font-medium  text-blueGray hover:bg-cyanDark hover:text-lightGray p-2 rounded-lg">
-                <IoIosCalendar className="mr-2  " /> Calendario
-              </span>
-            </Link>
-            <Link to="/tareas">
-              <span className="w-full h-10  flex items-center text-xl font-medium   text-blueGray hover:bg-cyanDark hover:text-lightGray p-2 rounded-lg">
-                <FaListUl className="mr-2  " /> Tareas
-              </span>
-            </Link>
-            <Link to="/clientes">
-              <span className="w-full h-10  flex items-center text-xl font-medium   text-blueGray hover:bg-cyanDark hover:text-lightGray p-2 rounded-lg">
-                <FaUsers className="mr-2  " /> Clientes
-              </span>
-            </Link>
+            <ButtonSideBar
+              text="Inicio"
+              icon={<AiOutlineHome className="mr-1" />}
+              isOpen={isOpen}
+              url="/dashboard"
+            />
+            <ButtonSideBar
+              text="Calendario"
+              icon={<IoIosCalendar className="mr-1" />}
+              isOpen={isOpen}
+              url="/calendarios"
+            />
+
+            <ButtonSideBar
+              text="Tareas"
+              icon={<FaListUl className="mr-1" />}
+              isOpen={isOpen}
+              url="/tareas"
+            />
+            <ButtonSideBar
+              text="Clientes"
+              icon={<FaUsers className="mr-1" />}
+              isOpen={isOpen}
+              url="/clientes"
+            />
           </div>
         </div>
+        <div className="w-full flex items-end justify-end">
+          <button onClick={toggleSidebar} className=" text-white text-4xl">
+            {isOpen ? <IoIosArrowForward /> : <IoIosArrowBack />}
+          </button>
+        </div>
+
         <div className="w-3/4 mb-5">
-          <div className="flex items-center gap-2 mb-2">
-            <div className=" overflow-hidden" />
-            <img
-              src="https://media.istockphoto.com/id/1386479313/es/foto/feliz-mujer-de-negocios-afroamericana-millennial-posando-aislada-en-blanco.jpg?s=612x612&w=0&k=20&c=JP0NBxlxG2-bdpTRPlTXBbX13zkNj0mR5g1KoOdbtO4="
-              alt=""
-              className="w-8 h-8 rounded-full"
-            />
-            <div>
-              <p className="text-sm font-medium text-[#779EBF]">Usuario</p>
-              <p className="text-xs text-cyanDark">usuario@ejemplo.com</p>
+          {isOpen ? (
+            <div className="flex items-center gap-2 mb-2">
+              <div className=" overflow-hidden" />
+              <img
+                src="https://media.istockphoto.com/id/1386479313/es/foto/feliz-mujer-de-negocios-afroamericana-millennial-posando-aislada-en-blanco.jpg?s=612x612&w=0&k=20&c=JP0NBxlxG2-bdpTRPlTXBbX13zkNj0mR5g1KoOdbtO4="
+                alt=""
+                className="w-8 h-8 rounded-full"
+              />
+              <div>
+                <p className="text-base font-medium text-[#779EBF]">Usuario</p>
+                <p className="text-base text-cyanDark">usuario@ejemplo.com</p>
+              </div>
             </div>
-          </div>
-          <Button
-            variant="ghost"
-            className="w-full justify-start text-[#779EBF] hover:text-[#5CE1E6] hover:bg-[#0097B2]/10"
-          >
-            <IoMdSettings className="mr-2 h-4 w-4" />
-            Configuración
-          </Button>
-          <Link to={"/inicio"}>
-            <Button
-              variant="ghost"
-              className="w-full justify-start text-[#779EBF] hover:text-[#5CE1E6] hover:bg-[#0097B2]/10"
-            >
-              <IoMdLogOut className="mr-2 h-4 w-4" />
-              Cerrar sesión
-            </Button>
-          </Link>
+          ) : (
+            <div className="w-full flex justify-center items-center mb-3">
+              <img
+                src="https://media.istockphoto.com/id/1386479313/es/foto/feliz-mujer-de-negocios-afroamericana-millennial-posando-aislada-en-blanco.jpg?s=612x612&w=0&k=20&c=JP0NBxlxG2-bdpTRPlTXBbX13zkNj0mR5g1KoOdbtO4="
+                alt=""
+                className="w-8 h-8 rounded-full"
+              />
+            </div>
+          )}
+
+          <ButtonSideBar
+            text="Configuración"
+            icon={<IoMdSettings className="mr-1" />}
+            isOpen={isOpen}
+            url="/configuración"
+          />
+          <ButtonSideBar
+            text=" Cerrar sesión"
+            icon={<IoMdLogOut className="mr-1" />}
+            isOpen={isOpen}
+            url="/singout"
+          />
         </div>
       </div>
     </>
