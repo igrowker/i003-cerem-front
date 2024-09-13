dd93d09291744231aafb22823575da0c0ea12ace
eliasgiannantonio <giannantonioelias@gmail.com>
2024-09-12 22:29:55 -0300

    Seccion features / infografía 2

---
+++ src/components/Features_2/index.tsx
@@ -0,0 +1,96 @@
+const Features_2 = () => {
+  return (
+    <div className="max-w-[100vw]">
+      <h3 className="my-20 text-5xl font-bold text-center text-blue-950">
+        We make it easy to track all data analytics
+      </h3>
+      <div className="flex flex-col items-center gap-10">
+        <div className="flex max-h-[370px] justify-between items-center px-28">
+          <div className="flex flex-col items-start gap-3 max-w-[400px]">
+            <img
+              src="src/assets/features-2/Icon1-l.svg"
+              alt="Icon"
+              className="h-12"
+            />
+            <p className="text-2xl font-bold text-blue-950">
+              Real Time Analytics
+            </p>
+            <p className="pr-24 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Laboriosam quasi numquam ab praesentium? Delectus quae quas
+              possimus magnam.
+            </p>
+            <button className="px-6 py-2 mt-4 text-white bg-purple-500 rounded-full hover:bg-purple-600">
+              Learn More
+            </button>
+          </div>
+          <div className="flex items-center">
+            <img
+              src="src/assets/features-2/Illustration1-r.svg"
+              className="w-auto h-auto"
+              alt="Illustration"
+            />
+          </div>
+        </div>
+
+        <div className="flex max-h-[370px] justify-between items-center px-28 flex-row-reverse">
+          <div className="flex flex-col items-start gap-3 max-w-[400px]">
+            <img
+              src="src/assets/features-2/Icon2-r.svg"
+              alt="Icon"
+              className="h-12"
+            />
+            <p className="text-2xl font-bold text-blue-950">
+              Real Time Analytics
+            </p>
+            <p className="pr-24 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Laboriosam quasi numquam ab praesentium? Delectus quae quas
+              possimus magnam.
+            </p>
+            <button className="px-6 py-2 mt-4 text-white bg-purple-500 rounded-full hover:bg-purple-600">
+              Learn More
+            </button>
+          </div>
+          <div className="flex items-center">
+            <img
+              src="src/assets/features-2/Illustration2-l.svg"
+              className="w-auto h-auto"
+              alt="Illustration"
+            />
+          </div>
+        </div>
+
+        <div className="flex max-h-[370px] justify-between items-center px-28">
+          <div className="flex flex-col items-start gap-3 max-w-[400px]">
+            <img
+              src="src/assets/features-2/Icon3-l.svg"
+              alt="Icon"
+              className="h-12"
+            />
+            <p className="text-2xl font-bold text-blue-950">
+              Real Time Analytics
+            </p>
+            <p className="pr-24 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Laboriosam quasi numquam ab praesentium? Delectus quae quas
+              possimus magnam.
+            </p>
+            <button className="px-6 py-2 mt-4 text-white bg-purple-500 rounded-full hover:bg-purple-600">
+              Learn More
+            </button>
+          </div>
+          <div className="flex items-center">
+            <img
+              src="src/assets/features-2/Illustration3-r.svg"
+              className="w-auto h-auto"
+              alt="Illustration"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Features_2;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Patrocinadores from "@/components/Patrocinadores";
 import Integrations from "@/components/Integrations";
+import Features_2 from "@/components/Features_2";
 
 
 const HomePage = () => {
@@ -9,6 +10,8 @@ const HomePage = () => {
       <Hero />
 
       <Patrocinadores/>
+      
+      <Features_2/>
 
       <div className="flex items-center justify-center w-full integrations">
         <Integrations />
