b791fd9c6564db1403ee6e72ac5fc58bb167c2cf
eliasgiannantonio <giannantonioelias@gmail.com>
2024-09-15 21:43:57 -0300

    Agrego componente de features-info-1

--- src/components/Features_2/index.tsx
+++ src/components/Features_2/index.tsx
@@ -1,20 +1,20 @@
 const Features_2 = () => {
   return (
-    <div className="max-w-[100vw] mx-auto px-8">
+    <div className="flex flex-col lg:max-w-[90vw]">
       {" "}
       <h3 className="my-20 text-5xl font-bold text-center text-blue-950">
         We make it easy to track all data analytics
       </h3>
       <div className="flex flex-col gap-20 pb-12">
         {" "}
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 lg:gap-40">
           <div className="flex flex-col items-start gap-3 max-w-[400px]">
             <img
               src="src/assets/features-2/Icon1-l.svg"
               alt="Icon"
               className="h-12"
             />
-            <p className="text-2xl font-bold text-blue-950">
+            <p className="text-2xl font-bold  text-sky-600">
               Real Time Analytics
             </p>
             <p className="pr-24 text-gray-500">
@@ -34,16 +34,14 @@ const Features_2 = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col-reverse md:flex-row-reverse justify-center items-center gap-10 md:gap-20">
+        <div className="flex flex-col-reverse md:flex-row-reverse justify-center items-center gap-10 md:gap-20 lg:gap-40">
           <div className="flex flex-col items-start gap-3 max-w-[400px] relative sm:left-12">
             <img
               src="src/assets/features-2/Icon2-r.svg"
               alt="Icon"
               className="h-12"
             />
-            <p className="text-2xl font-bold text-blue-950">
-              Real Time Analytics
-            </p>
+            <p className="text-2xl font-bold  text-sky-600">Safely security</p>
             <p className="pr-24 text-gray-500">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Laboriosam quasi numquam ab praesentium? Delectus quae quas
@@ -61,16 +59,14 @@ const Features_2 = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20 lg:gap-40">
           <div className="flex flex-col items-start gap-3 max-w-[400px]">
             <img
               src="src/assets/features-2/Icon3-l.svg"
               alt="Icon"
               className="h-12"
             />
-            <p className="text-2xl font-bold text-blue-950">
-              Real Time Analytics
-            </p>
+            <p className="text-2xl font-bold text-sky-600">Multiple platform</p>
             <p className="pr-24 text-gray-500">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit.
               Laboriosam quasi numquam ab praesentium? Delectus quae quas
