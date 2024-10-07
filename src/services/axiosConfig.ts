52e47dedd8778943a5e7e261637492c6b1294e29
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-07 16:50:35 -0300

    creo el axios config

---
+++ src/services/axiosConfig.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const url = 'https://i003-cerem-back.onrender.com/'
+
+const axiosInstance = axios.create({
+    baseURL: url,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+// axiosInstance.interceptors.request.use(
+//     (config) => {
+//         const token = localStorage.getItem("authToken");
+
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+export default axiosInstance;
