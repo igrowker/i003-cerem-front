3493bc85b44d5d5715ba58dd732a5e62842d1f09
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-10 19:53:47 -0300

    toast

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n/i18n";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
+import { Toaster } from "sonner";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
+        <Toaster position="top-center" />
         <App />
       </Provider>
     </QueryClientProvider>
