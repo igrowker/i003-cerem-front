78ba623f0ad625e0ccaa0fb53f5f4c8b1f36823e
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-10 19:25:04 -0300

    redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import "./styles/index.css";
 import App from "./routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./i18n/i18n";
-
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </React.StrictMode>
 );
