6b72b5124ebcdf739882f961dd2147fca3d9b662
FRAN-PC\Francisco <fran.albert.sist@gmail.com>
2024-10-10 19:54:03 -0300

    axios bearer auth

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -9,19 +9,19 @@ const axiosInstance = axios.create({
     },
 });
 
-// axiosInstance.interceptors.request.use(
-//     (config) => {
-//         const token = localStorage.getItem("authToken");
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("authToken");
 
-//         if (token) {
-//             config.headers.Authorization = `Bearer ${token}`;
-//         }
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
 
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default axiosInstance;
